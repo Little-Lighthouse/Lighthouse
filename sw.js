461b3cc4bc7f453b68a5d60deb84c33cabca26ee
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2023-07-31 19:34:21 +0630

    Added images for Time Management and updated sw version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v-7_31_23-2';
-const dynamicCacheName = 'site-dynamic-v-7_31_23-2';
+const staticCacheName = 'site-static-v-7_31_23-3';
+const dynamicCacheName = 'site-dynamic-v-7_31_23-3';
 const assets = [
   '/Lighthouse/',
   '/Lighthouse/index.html',
