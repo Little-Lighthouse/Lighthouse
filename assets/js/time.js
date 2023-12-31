6c1b4a7ed95f4ff88ce2b1b94afb83915b4df801
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2023-12-31 23:01:47 +0630

    Delete assets directory

--- assets/js/time.js
+++
@@ -1,8 +0,0 @@
-let mmr = document.getElementById("mmrTime")
-
-setInterval(() => {
-    let d = new Date();
-    mmr.innerHTML = d.toLocaleTimeString("en-US",{timeZone:'Asia/Yangon',timeStyle:'medium'});
-},1000)
-
-
