35416122bc680e37590806a4852d0e0fbf74eb41
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2024-07-20 14:37:53 +0630

    Minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,69 +27,53 @@ $(document).ready(function(){
 
 /* ---- CHOOSING TOPIC (POSTS) ---- */
 $(document).ready(function(){
-
-  $('#topicType').on('change', function() {
+  $('#topicTypeCurrent').on('change', function() {
     if ( this.value == 'All'){
-      $(".education").show();
-      $(".NewsForYouth").show();
-      $(".motivation").show();
-      $(".entertainment").show();
-      $(".mentalPsycho").show();
-      $(".EngSetoutPete").show();
+      $(".EFY_Post").show();
+      $(".NFY_Post").show();
+      $(".EY_Post").show();
+      $(".VFM_Post").show();
+      $(".ESP_Post").show();
     }
 
-    if ( this.value == 'Education'){
-      $(".education").show();
-      $(".NewsForYouth").hide();
-      $(".motivation").hide();
-      $(".entertainment").hide();
-      $(".mentalPsycho").hide();
-      $(".EngSetoutPete").hide();
+    if ( this.value == 'EFY_Opt'){
+      $(".EFY_Post").show();
+      $(".NFY_Post").hide();
+      $(".EY_Post").hide();
+      $(".VFM_Post").hide();
+      $(".ESP_Post").hide();
     }
 
-    if ( this.value == 'Motivation'){
-      $(".education").hide();
-      $(".NewsForYouth").hide();
-      $(".motivation").show();
-      $(".entertainment").hide();
-      $(".mentalPsycho").hide();
-      $(".EngSetoutPete").hide();
+    if ( this.value == 'NFY_Opt'){
+      $(".EFY_Post").hide();
+      $(".NFY_Post").show();
+      $(".EY_Post").hide();
+      $(".VFM_Post").hide();
+      $(".ESP_Post").hide();
     }
 
-    if ( this.value == 'Entertainment'){
-      $(".education").hide();
-      $(".NewsForYouth").hide();
-      $(".motivation").hide();
-      $(".entertainment").show();
-      $(".mentalPsycho").hide();
-      $(".EngSetoutPete").hide();
+    if ( this.value == 'VFM_Opt'){
+      $(".EFY_Post").hide();
+      $(".NFY_Post").hide();
+      $(".EY_Post").hide();
+      $(".VFM_Post").show();
+      $(".ESP_Post").hide();
     }
 
-    if ( this.value == 'MentPsy'){
-      $(".education").hide();
-      $(".NewsForYouth").hide();
-      $(".motivation").hide();
-      $(".entertainment").hide();
-      $(".mentalPsycho").show();
-      $(".EngSetoutPete").hide();
+    if ( this.value == 'EY_Opt'){
+      $(".EFY_Post").hide();
+      $(".NFY_Post").hide();
+      $(".EY_Post").show();
+      $(".VFM_Post").hide();
+      $(".ESP_Post").hide();
     }
 
-    if ( this.value == 'News For Youth'){
-      $(".education").hide();
-      $(".NewsForYouth").show();
-      $(".motivation").hide();
-      $(".entertainment").hide();
-      $(".mentalPsycho").hide();
-      $(".EngSetoutPete").hide();
-    }
-
-    if ( this.value == 'Eng Setout Pete'){
-      $(".education").hide();
-      $(".NewsForYouth").hide();
-      $(".motivation").hide();
-      $(".entertainment").hide();
-      $(".mentalPsycho").hide();
-      $(".EngSetoutPete").show();
+    if ( this.value == 'ESP_Opt'){
+      $(".EFY_Post").hide();
+      $(".NFY_Post").hide();
+      $(".EY_Post").hide();
+      $(".VFM_Post").hide();
+      $(".ESP_Post").show();
     }
   });
 });
